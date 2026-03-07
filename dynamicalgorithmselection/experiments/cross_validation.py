ca9e71646a6d96a078bead8def6db302460af39b
wniec <niecwladek@gmail.com>
2026-03-07 17:37:08 +0100

    adjust experiments length

--- dynamicalgorithmselection/experiments/cross_validation.py
+++ dynamicalgorithmselection/experiments/cross_validation.py
@@ -25,7 +25,7 @@ def run_cross_validation(
         os.mkdir(results_dir)
     cocoex.utilities.MiniPrint()
     problems_suite, cv_folds = _get_cv_folds(
-        4 if leaving_mode != "LODO" else 3, leaving_mode, options.get("dimensionality")
+        3, leaving_mode, options.get("dimensionality")
     )
     # cv_folds is a tuple (train_set, test_set) for each fold
     # using len(cv_folds[0]) was a bug, because it was always 2
@@ -63,7 +63,7 @@ def _get_cv_folds(
 ):
     """
     :param n:  number of cross validation folds
-    :param is_loio: boolean to indicate how train and test sets should be split (leave-instance-out/leave-problem-out).
+    :param leaving_mode: str to indicate how train and test sets should be split (leave-instance-out/leave-problem-out/leave-dimension-out).
     :param dim: dimensionality of the problems. None indicates all of them.
     :return suite, list of (train set, test set) pairs:
     """
