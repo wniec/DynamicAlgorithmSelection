1143592c260d470efae4a1b49b66fed14c07d33f
WojtAcht <wachtelik@gmail.com>
2026-04-11 22:49:16 +0200

    feat: simplify init

--- dynamicalgorithmselection/analysis/__init__.py
+++ dynamicalgorithmselection/analysis/__init__.py
@@ -21,12 +21,6 @@ __all__ = [
     "load_ert_htmls",
     "load_action_sequences",
     "ActionSequence",
-    "ActionProbabilityPlotter",
-    "generate_action_probability_report",
-    "DiversitySequence",
-    "DiversityAnalyser",
-    "load_diversity_sequences",
-    "generate_diversity_report",
     "aggregate_over_seeds",
     "extract_seed_info",
     "split_results_by_dimension",
@@ -35,20 +29,3 @@ __all__ = [
     "parse_ert_from_html",
     "compute_solve_rate",
 ]
-
-
-def __getattr__(name: str):
-    if name in {"ActionProbabilityPlotter", "generate_action_probability_report"}:
-        from dynamicalgorithmselection.analysis import action_probabilities
-
-        return getattr(action_probabilities, name)
-    if name in {
-        "DiversitySequence",
-        "DiversityAnalyser",
-        "load_diversity_sequences",
-        "generate_diversity_report",
-    }:
-        from dynamicalgorithmselection.analysis import diversity_analysis
-
-        return getattr(diversity_analysis, name)
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
